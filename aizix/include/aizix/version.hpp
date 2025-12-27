0f75f823a2d6a65828a6f270048f4c736ed7a1f0
aizige <aizige@gmail.com>
2025-12-27 11:56:17 +0800

    update version

--- aizix/include/aizix/version.hpp
+++ aizix/include/aizix/version.hpp
@@ -7,9 +7,8 @@
 
 
 namespace aizix::framework {
-    // 使用 std::string_view 避免不必要的字符串拷贝
     constexpr std::string name = "Aizix";
     // 宏会在编译时被替换为 CMake 中定义的值
-    constexpr std::string version = "1.0";
+    constexpr std::string version = "3.2.0";
 }
 #endif //AIZIX_VERSION_HPP
